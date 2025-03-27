2ce8aa1e35402adc425b29a38623aa652322d32e
linyueCS-IT <6197673@johnabbottcollege.net>
2025-03-26 23:32:29 -0400

    Implement 3.2-useEffect

--- exercises/3.2-react-useState/client/src/App.jsx
+++ exercises/3.2-react-useState/client/src/App.jsx
@@ -3,64 +3,64 @@ import "./App.css";
 import PokemonList from "./components/PokemonList";
 import MainHeader from "./components/MainHeader";
 import { useState } from "react";
-
+import Counter from "./components/Counter";
 
 function App() {
 	const [myPokemons, setMyPokemons] = useState([
 		"Charmander",
 		"Bulbasaur",
 		"Squirtle",
-		"Rhydon"
+		"Rhydon",
 	]);
-	//event handler to add new value to existing array of pokemons  
+	//event handler to add new value to existing array of pokemons
 	const addPokemon = (newPokemon) => {
 		setMyPokemons([...myPokemons, newPokemon]);
 	};
-	const changeColor = () =>{
-		const colors = ["white","steelblue","pink", "yellow","purple"];
+	const changeColor = () => {
+		const colors = ["white", "steelblue", "pink", "yellow", "purple"];
 		const randomColor = colors[Math.floor(Math.random() * colors.length)];
 		// setBgColor("pink");
 		setBgColor(randomColor);
-	}
-	const [name, setName] = useState("Charmander");
-	const [count, setCount] = useState(0);
-	const [newPokemon,setNewPokemon] = useState("Ivysauri");
-	const [bgColor, setBgColor] = useState("white");
-	
-	const eventHandler = (e) =>{
-		setName(e.target.value)
 	};
-	
-	
+	const [name, setName] = useState("Charmander");
+	// Example 1.Using useState for a Counter
+	const [count, setCount] = useState(0);
+	const [newPokemon, setNewPokemon] = useState("Ivysauri");
+	const [bgColor, setBgColor] = useState("white");
+
+	const eventHandler = (e) => {
+		setName(e.target.value);
+	};
+
 	return (
-		<div className="container"
-			style={{backgroundColor: bgColor, padding: "20px"}}
+		<div
+			className="container"
+			style={{ backgroundColor: bgColor, padding: "20px" }}
 		>
+			{/* <h2>Counter: {count}</h2> */}
+			<Counter count={count} />
+			<button onClick={() => setCount(count + 1)}>Increase</button>
+			<button onClick={() => setCount(count - 1)}>Decrease</button>
 			<article>
 				<hgroup>
 					<div>
-						<MainHeader /> {" "}
+						<MainHeader />{" "}
 					</div>
 				</hgroup>
 				<h2>Meet my Pokemon: {name}</h2>
-				<input
-					type="text"
-					value={name}
-					onChange={eventHandler}
-				/>
-				<h3> Counter: {count} </h3>		
+				<input type="text" value={name} onChange={eventHandler} />
+
 			</article>
-			<button onClick={() => setCount(count + 1)}>Increase</button>
-			<PokemonList pokemons={myPokemons}/>
+			<PokemonList pokemons={myPokemons} />
 			<article>
 				<input
 					type="text"
 					value={newPokemon}
 					onChange={(e) => setNewPokemon(e.target.value)}
 				/>
-			</article>			
-			<button onClick={() =>addPokemon(newPokemon)}>Add Pokemon</button>
-			<button onClick={() =>changeColor()}>Change Color</button>
+			</article>
+			<button onClick={() => addPokemon(newPokemon)}>Add Pokemon</button>
+			<button onClick={() => changeColor()}>Change Color</button>
 		</div>
 	);
 }
--- exercises/3.2-react-useState/client/src/components/Greeting.jsx
+++ exercises/3.2-react-useState/client/src/components/Greeting.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { useState } from "react";
 
-function Greeting(props){
+// Example 2 : Changing a HTML element Add a toggle function to change the colour of the
+// text to red if blue, and vice versa. Watch the change using Inspect on the browser.
+function Greeting(props) {
 	const [color, setColor] = useState("green");
-	const toggle = (color) =>{
-		return color === "blue"? "red" : "blue"
+	const toggle = (color) => {
+		return color === "orange" ? "green" : "orange";
 	};
 
 	return (
 		<div>
-			<h2 style={{color : color}}> Meet my {props.name} </h2>
-    			<button onClick={() => setColor(toggle(color))}>
-                Toggle Color
-    		</button>
+			<h2 style={{ color: color }}> Meet my {props.name} </h2>
+			<button onClick={() => setColor(toggle(color))}>
+				Toggle Color
+			</button>
 		</div>
-	);	
+	);
 }
 export default Greeting;
